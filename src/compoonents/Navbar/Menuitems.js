d821b2d51a21f5a0ddb9264de23a2c612f533db6
jimakosk/todolist <jkon258@gmail.com>
2021-08-15 12:52:49 +0300

    a

---
+++ src/compoonents/Navbar/Menuitems.js
@@ -0,0 +1,35 @@
+ export const MenuItems= [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links'
+    
+  },
+    {
+      title: 'Services',
+      url: '#',
+      cName: 'nav-links'
+      
+    },
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'nav-links'
+        
+    }, 
+     {
+          title: 'Contact Us',
+          url: '#',
+          cName: 'nav-links'
+          
+      }, 
+        
+       {
+            title: 'Sing up',
+            url: '#',
+            cName: 'nav-links'
+            
+          }
+  
+  
+]
